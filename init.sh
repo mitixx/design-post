73a2ecba514820d567dc18fb2fa13242a3e72a11
未知 <haruokame05@yahoo.co.jp>
2018-03-04 21:44:30 +0900

    provisional

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
-     sudo apt-get update && sudo apt-get upgrade -y
-     sudo apt-get install -y python3-pip python3-dev
-     sudo pip install Django
-     curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-     sudo apt-get install -y nodejs
-     sudo apt-get install -y mysql-serve
+     sudo apt-get update && sudo apt-get upgrade -y\
+     sudo apt-get install -y python3-pip python3-dev\
+     pip3 install --upgrade pip\
+     sudo pip3 install PyMySQL Django\
+     curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -\
+     sudo apt-get install -y nodejs\
+     sudo apt-get install -y mysql-serve\
